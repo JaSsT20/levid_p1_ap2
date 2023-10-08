40df1860a2a4d2a2230ff5f57d9228779e0605a2
Levid <jassontejada@gmail.com>
2023-10-08 10:41:39 -0400

    Updating dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,8 +81,11 @@ dependencies {
     //Datastore
     implementation("androidx.datastore:datastore-preferences:1.0.0")
 
-    //lifecycle
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    val lifeCycleVersion = "2.6.2"
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifeCycleVersion")
+    implementation ("androidx.lifecycle:lifecycle-runtime-compose:$lifeCycleVersion")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifeCycleVersion")
+
     //para hiltViewModel()
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
 }
